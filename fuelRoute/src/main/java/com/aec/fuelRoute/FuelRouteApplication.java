f9028d7299e3654d58cec993eb0951a148d269ff
Adrian Edgardo Camus <adrianedgardocamus@gmail.com>
2024-07-19 19:48:20 -0300

    first commit

---
+++ fuelRoute/src/main/java/com/aec/fuelRoute/FuelRouteApplication.java
@@ -0,0 +1,13 @@
+package com.aec.fuelRoute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FuelRouteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FuelRouteApplication.class, args);
+	}
+
+}
